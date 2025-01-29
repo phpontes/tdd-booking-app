78f386e1d203f2337d6087578c30c0e06f2dfff3
phpontes <ppontes95@hotmail.com>
2025-01-29 07:01:20 -0300

    Testando regras de negócio

--- src/domain/entities/property.test.ts
+++ src/domain/entities/property.test.ts
@@ -12,8 +12,29 @@ describe("Property Entity", () => {
 
     expect(property.getId()).toBe("1");
     expect(property.getName()).toBe("Casa de praia");
-    expect(property.getDescription()).toBe("Uma baita casona de frente ca praia, bicho!");
+    expect(property.getDescription()).toBe(
+      "Uma baita casona de frente ca praia, bicho!"
+    );
     expect(property.getMaxGuests()).toBe(4);
     expect(property.getBasePricePerNight()).toBe(200);
+  });
+
+  it("deve lançar um erro se o nome for vazio", () => {
+    expect(() => {
+      new Property("1", "", "Descrição", 4, 200);
+    }).toThrow("O nome é obrigatório");
+  });
+
+  it("deve lançar um erro se o número máximo de hóspedes for zero ou negativo", () => {
+    expect(() => {
+      new Property("1", "Casa", "Descrição", 0, 200);
+    }).toThrow("O número máximo de hóspedes deve ser maior que zero");
+  });
+
+  it("deve validar o número máximo de hóspedes", () => {
+    const property = new Property("1", "Casa de Campo", "Descrição", 5, 150);
+    expect(() => {
+      property.validateGuestCount(6);
+    }).toThrow("Número de hóspedes excede o limite. Limite: 5");
   });
 });
--- src/domain/entities/property.ts
+++ src/domain/entities/property.ts
@@ -6,6 +6,12 @@ export class Property {
     private maxGuests: number,
     private basePricePerNight: number
   ) {
+    if (!name) {
+      throw new Error("O nome é obrigatório");
+    }
+    if (maxGuests <= 0) {
+      throw new Error("O número máximo de hóspedes deve ser maior que zero");
+    }
     this.id = id;
     this.name = name;
     this.description = description;
@@ -31,5 +37,13 @@ export class Property {
 
   getBasePricePerNight(): number {
     return this.basePricePerNight;
+  }
+
+  validateGuestCount(guestCount: number): void {
+    if (guestCount > this.maxGuests) {
+      throw new Error(
+        "Número de hóspedes excede o limite. Limite: " + this.maxGuests + "."
+      );
+    }
   }
 }
