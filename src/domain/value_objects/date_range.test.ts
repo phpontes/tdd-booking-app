12740c229960ff4c54404976209988ca365ca8cc
phpontes <ppontes95@hotmail.com>
2025-03-05 08:30:27 -0300

    iniciando pelo range de datas

---
+++ src/domain/value_objects/date_range.test.ts
@@ -0,0 +1,28 @@
+import { DateRange } from "./date_range";
+
+describe("DateRange Value Object", () => {
+  it("deve lançar um erro se a data de término for antes da data de início", () => {
+    expect(() => {
+      new DateRange(new Date("2024-12-25"), new Date("2024-12-20"));
+    }).toThrow("A data de término deve ser posterior à data de início");
+  });
+
+  it("deve criar uma instância de DateRange com a data de início e data de término, e verificar o retorno das datas", () => {
+    const startDate = new Date("2024-12-20");
+    const endDate = new Date("2024-12-25");
+    const dateRange = new DateRange(startDate, endDate);
+
+    expect(dateRange.getStartDate()).toEqual(startDate);
+    expect(dateRange.getEndDate()).toEqual(endDate);
+  });
+
+  it("deve calcular o total de noites corretamente", () => {
+    const startDate = new Date("2024-12-20");
+    const endDate = new Date("2024-12-25");
+    const dateRange = new DateRange(startDate, endDate);
+
+    const totalNights = dateRange.getTotalNights();
+
+    expect(totalNights).toBe(5);
+  });
+});
---
+++ src/domain/value_objects/date_range.ts
@@ -0,0 +1,20 @@
+export class DateRange {
+  private readonly startDate: Date;
+  private readonly endDate: Date;
+
+  constructor(startDate: Date, endDate: Date) {
+    if (endDate <= startDate) {
+      throw new Error("A data de término deve ser posterior à data de início");
+    }
+    this.startDate = startDate;
+    this.endDate = endDate;
+  }
+
+  getStartDate(): Date {
+    return this.startDate;
+  }
+
+  getEndDate(): Date {
+    return this.endDate;
+  }
+}
