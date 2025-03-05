8d49623f746c970e6fc6c27e92498bd03641bb09
phpontes <ppontes95@hotmail.com>
2025-03-05 10:09:11 -0300

    testando regras de negócio

--- src/domain/entities/property.test.ts
+++ src/domain/entities/property.test.ts
@@ -16,4 +16,24 @@ describe("Property Entity", () => {
     expect(property.getMaxGuests()).toBe(4);
     expect(property.getBasePricePerNight()).toBe(200);
   });
+
+  it("deve lançar um erro se o nome for vazio", () => {
+    expect(() => {
+      new Property("1", "", "Descrição", 4, 200);
+    }).toThrow("O nome é obrigatório");
+  });
+
+  it("deve lançar um erro se o número máximo de hóspedes for zero ou negativo", () => {
+    expect(() => {
+      new Property("1", "Casa", "Descrição", 0, 200);
+    }).toThrow("O número máximo de hóspedes deve ser maior que zero");
+  });
+
+  it("deve validar o número máximo de hóspedes", () => {
+    const property = new Property("1", "Casa", "Descrição", 5, 150);
+    
+    expect(() => {
+        property.validateGuestCount(6);
+    }).toThrow("Número máximo de hóspedes excedido. Máximo permitido: 5");
+  });
 });
--- src/domain/entities/property.ts
+++ src/domain/entities/property.ts
@@ -6,6 +6,12 @@ export class Property {
     private maxGuests: number,
     private basePricePerNight: number
   ) {
+    if (!name) {
+      throw new Error("O nome é obrigatório");
+    }
+    if (maxGuests <= 0) {
+      throw new Error("O número máximo de hóspedes deve ser maior que zero");
+    }
     this.id = id;
     this.name = name;
     this.description = description;
@@ -13,23 +19,31 @@ export class Property {
     this.basePricePerNight = basePricePerNight;
   }
 
-    getId(): string {
-        return this.id;
-    }
+  getId(): string {
+    return this.id;
+  }
 
-    getName(): string {
-        return this.name;
-    }
+  getName(): string {
+    return this.name;
+  }
 
-    getDescription(): string {
-        return this.description;
-    }
+  getDescription(): string {
+    return this.description;
+  }
 
-    getMaxGuests(): number {
-        return this.maxGuests;
-    }
+  getMaxGuests(): number {
+    return this.maxGuests;
+  }
 
-    getBasePricePerNight(): number {
-        return this.basePricePerNight;
+  getBasePricePerNight(): number {
+    return this.basePricePerNight;
+  }
+
+  validateGuestCount(guestCount: number): void {
+    if (guestCount > this.maxGuests) {
+      throw new Error(
+        `Número máximo de hóspedes excedido. Máximo permitido: ${this.maxGuests}.`
+      );
     }
+  }
 }
