12a27544a1c0b16f58d464f155fa66c0dbcfab11
phpontes <ppontes95@hotmail.com>
2025-03-07 10:55:20 -0300

    implementando create booking

--- src/application/services/booking_service.ts
+++ src/application/services/booking_service.ts
@@ -1,8 +1,42 @@
 import { CreateBookingDTO } from "../dtos/create_booking_dto";
 import { Booking } from "../../domain/entities/booking";
+import { BookingRepository } from "../../domain/repositories/booking_repository";
+import { PropertyService } from "./property_service";
+import { UserService } from "./user_service";
+import { DateRange } from "../../domain/value_objects/date_range";
+import { v4 as uuidv4 } from "uuid";
 
 export class BookingService {
-    async createBooking(dto: CreateBookingDTO): Promise<Booking> {
-        
+  constructor(
+    private readonly bookingRepository: BookingRepository,
+    private readonly propertyService: PropertyService,
+    private readonly userService: UserService
+  ) {}
+
+  async createBooking(dto: CreateBookingDTO): Promise<Booking> {
+    const property = await this.propertyService.findPropertyById(
+      dto.propertyId
+    );
+    if (!property) {
+      throw new Error("Propriedade não encontrada");
     }
+
+    const guest = await this.userService.findUserById(dto.guestId);
+    if (!guest) {
+      throw new Error("Usuário não encontrado");
+    }
+
+    const dateRange = new DateRange(dto.startDate, dto.endDate);
+
+    const booking = new Booking(
+      uuidv4(),
+      property,
+      guest,
+      dateRange,
+      dto.guestCount
+    );
+
+    await this.bookingRepository.save(booking);
+    return booking;
+  }
 }
