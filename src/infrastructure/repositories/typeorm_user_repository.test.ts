037d011423a046bd9ec53cfde6cd09f3e717ff66
phpontes <ppontes95@hotmail.com>
2025-03-13 10:30:56 -0300

    iniciando repositório com banco de dados - typeorm & sqlite dependencies

---
+++ src/infrastructure/repositories/typeorm_user_repository.test.ts
@@ -0,0 +1,36 @@
+import { DataSource, Repository, TypeORMError } from "typeorm";
+import { User } from "../../domain/entities/user";
+
+describe("TypeORMUserRepository", () => {
+  let dataSource: DataSource;
+  let userRepository: TypeORMUserRepository;
+  let repository: Repository<UserEntity>;
+
+  beforeAll(async () => {
+    dataSource = new DataSource({
+      type: "sqlite",
+      database: ":memory:",
+      dropSchema: true,
+      entities: [UserEntity],
+      synchronize: true,
+      logging: false,
+    });
+    await dataSource.initialize();
+    repository = dataSource.getRepository(UserEntity);
+    userRepository = new TypeORMUserRepository(repository);
+  });
+
+  afterAll(async () => {
+    await dataSource.destroy();
+  });
+
+  it("deve salvar um usuário com sucesso", async () => {
+    const user = new User("1", "José das Couves");
+    await userRepository.save(user);
+
+    const savedUser = await userRepository.findOne({ where: { id: "1" } });
+    expect(savedUser).not.toBeNull();
+    expect(savedUser?.id).toBe("1");
+    expect(savedUser?.name).toBe("John Doe");
+  });
+});
